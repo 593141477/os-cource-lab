942958495d69794a8b3197557a7245bc5e8c3d50
张宇翔 <zz593141477@gmail.com>
2014-08-09 10:52:13 +0800

    exercise 2 - complete FIFO page replacement algorithm

--- kern/mm/swap_fifo.c
+++ kern/mm/swap_fifo.c
@@ -49,8 +49,8 @@ _fifo_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int
  
     assert(entry != NULL && head != NULL);
     //record the page access situlation
-    /*LAB3 EXERCISE 2: YOUR CODE*/ 
-    //(1)link the most recent arrival page at the back of the pra_list_head qeueue.
+    /*LAB3 EXERCISE 2: 2013011352*/ 
+    list_add_before(head, entry); //(1)link the most recent arrival page at the back of the pra_list_head qeueue.
     return 0;
 }
 /*
@@ -64,9 +64,11 @@ _fifo_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick
          assert(head != NULL);
      assert(in_tick==0);
      /* Select the victim */
-     /*LAB3 EXERCISE 2: YOUR CODE*/ 
-     //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
-     //(2)  set the addr of addr of this page to ptr_page
+     /*LAB3 EXERCISE 2: 2013011352*/ 
+     list_entry_t *victim = list_next(head);
+     assert(victim != head);
+     list_del(victim);  //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
+     *ptr_page = le2page(victim, pra_page_link); //(2)  set the addr of addr of this page to ptr_page
      return 0;
 }
 
